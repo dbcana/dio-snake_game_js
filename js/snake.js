25880ace290b2fe524e74133de0ea8a634679d87
Daniel Borba Caña <dbcana.unique@gmail.com>
2021-05-02 21:07:01 -0300

    Controle de velocidade #1

--- js/snake.js
+++ js/snake.js
@@ -1,24 +1,20 @@
-let canvas = document.getElementById('snake'); 
+let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 
 let dimension = 20;
 let box = 20;
-let speed = 400;
 canvas.width = dimension * box;
 canvas.height = dimension * box;
 
-let snake = []; 
-snake[0] = {
-    x: dimension / 2 * box,
-    y: dimension / 2 * box
-};
+let speed = 400;
 let direction = 'right';
-
+let snake = [];
 let food = {};
+let game;
 
 function drawBackground(){
     context.fillStyle = 'lightgreen';
-    context.fillRect(0, 0, dimension*box, dimension*box);    
+    context.fillRect(0, 0, dimension*box, dimension*box);
 }
 
 function drawSnake (){
@@ -41,14 +37,61 @@ function newFood(){
 }
 
 function keyPressed(event){
-    if(event.keyCode == 37 && direction != 'right') direction = 'left';
-    if(event.keyCode == 38 && direction != 'down') direction = 'up';
-    if(event.keyCode == 39 && direction != 'left') direction = 'right';
-    if(event.keyCode == 40 && direction != 'up') direction = 'down';
+    if(event.keyCode == 37 && direction != 'right') changeDirection('left');
+    if(event.keyCode == 38 && direction != 'down') changeDirection('up');
+    if(event.keyCode == 39 && direction != 'left') changeDirection('right');
+    if(event.keyCode == 40 && direction != 'up') changeDirection('down');
+    if(event.key === '+') increseSpeed();
+    if(event.key === '-') decreseSpeed();
+}
+
+function changeDirection(dir){
+    direction = dir;
     refreshGame();
 }
 
-function refreshGame(){
+function increseSpeed(){
+    if(speed >= 100){
+        speed = speed - 50;
+        clearInterval(game);
+        game = setInterval(refreshGame, speed);
+    }
+}
+
+function decreseSpeed(){
+    if(speed <= 400){
+        speed = speed + 50;
+        clearInterval(game);
+        game = setInterval(refreshGame, speed);
+    }
+}
+
+function checkColision(){
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('Game Over');
+            resetGame();
+        }
+    }
+}
+
+function checkOverload(){
+    if(direction == 'right' && snake[0].x >= dimension * box){
+        snake[0].x = 0;
+    } 
+    if(direction == 'left' && snake[0].x < 0){
+        snake[0].x = (dimension - 1) * box;
+    }
+    if(direction == 'down' && snake[0].y >= dimension * box){
+        snake[0].y = 0;
+    }
+    if(direction == 'up' && snake[0].y < 0){
+        snake[0].y = (dimension - 1) * box;
+    }
+}
+
+function checkMove(){
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -62,39 +105,35 @@ function refreshGame(){
     }else{
         newFood();
     }
-    
+
     let newHead ={
         x: snakeX,
         y: snakeY
     }
-
     snake.unshift(newHead);
+}
 
-    if(direction == 'right' && snake[0].x >= dimension * box){
-        snake[0].x = 0;
-    } 
-    if(direction == 'left' && snake[0].x < 0){
-        snake[0].x = (dimension - 1) * box;
-    }
-    if(direction == 'down' && snake[0].y >= dimension * box){
-        snake[0].y = 0;
-    }
-    if(direction == 'up' && snake[0].y < 0){
-        snake[0].y = (dimension - 1) * box;
-    }
-    
-    for(i = 1; i < snake.length; i++){
-        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-            clearInterval(game);
-            alert('Game Over');
-        }
-    }
-
+function refreshGame(){
+    checkMove();
+    checkOverload();
+    checkColision();
     drawBackground();
     drawSnake();
     drawFood();
 }
 
+function resetGame(){
+    newFood();
+    speed = 400;
+    snake = []; 
+    snake[0] = {
+        x: dimension / 2 * box,
+        y: dimension / 2 * box
+    };
+    direction = 'right';
+    clearInterval(game);
+    game = setInterval(refreshGame, speed);
+}
+
+resetGame();
 document.addEventListener('keydown', keyPressed);
-newFood();
-let game = setInterval(refreshGame, speed);
